625f63d27b923a3e1aacbc1709c8d3371a97a99f
Petr <pedrossereda@gmail.com>
2026-07-25 23:54:03 +0300

    [ADD] - Information in AboutWindow

--- src/components/AboutWindow.tsx
+++ src/components/AboutWindow.tsx
@@ -35,6 +35,50 @@ function AboutWindow({ onClose, position }: Props){
               <p>Статус: <span>в поиске работы</span></p>
             </div>
           </div>
+          <div className="about__body">
+            <div className="about__body--list">
+              <p>привет! я пётр, я fullstack разработчик </p>
+              <ul>
+                <li>разрабатываю веб-приложения и backend-сервисы</li>
+                <li>создаю интерфейсы и прототипы в figma</li>
+                <li>работаю с современными web-технологиями (react, typescript)</li>
+                <li>интересуюсь архитектурой приложений и безопасностью</li>
+              </ul>
+            </div>
+            <div className="about__body--list">
+              <h4>опыт работы</h4>
+              <div className="special__сase" >
+                <p className="name">PharmaTrace</p>
+                <p className="comment">Junior IT Support Specialist (with development tasks)</p>
+                <p className="comment">Нояб. 2024 - настоящие время</p>
+              </div>
+            </div>
+            <div className="about__body--list">
+              <h4>ОБРОЗОВАНИЕ</h4>
+              <div className="special__сase" >
+                <p className="name">МПТ РЭУ им. Г.В. Плеханова</p>
+                <p className="comment">09.02.07 Разработчик веб и мультимейдийных приложений</p>
+                <p className="comment">(ОКОНЧИЛ С ОТЛИЧИЕМ В 2025 году)</p>
+              </div>
+            </div>
+            <div className="about__body--list">
+              <h4>ДРУГИЕ ИНТЕРЕСЫ</h4>
+              <ul>
+                <li>разработка личных проектов и изучение новых технологий</li>
+                <li>изучение безопасности приложений и backend-систем</li>
+                <li>изучение испанского языка</li>
+                <li>походы и активный отдых</li>
+                <li>учусь играть на гитаре</li>
+                <li>волейбол</li>
+              </ul>
+            </div>
+            <div className="about__body--list">
+              <h4>владение языками</h4>
+              <div className="special__сase" >
+                <p className="name">Я разговариваю на <span>английском</span> (B2) и <span>испанском</span> (B1) языках</p>
+              </div>
+            </div>
+          </div>
         </WindowContainer>
       </div>
     </>
